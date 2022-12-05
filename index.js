1cc9b7f27d06c088566e3b1077b6473e8ee8624c
Sean Zellmer <sean@lejeunerenard.com>
2022-12-05 00:37:32 -0800

    feat: Implement naive `emit()` & `on()` based on hyperbee index

---
+++ index.js
@@ -0,0 +1,117 @@
+import { EventEmitter } from 'events'
+import codecs from 'codecs'
+import b4a from 'b4a'
+import Autobase from 'autobase'
+import Hyperbee from 'hyperbee'
+import assert from 'assert'
+
+export class EventBus {
+  constructor (corestore, opts = {}) {
+    this.keyEncoding = opts.keyEncoding ? codecs(opts.keyEncoding) : null
+    // TODO Decide if i want to default valueEncoding to json or something else
+    this.valueEncoding = opts.valueEncoding ? codecs(opts.valueEncoding) : null
+
+    // TODO decide whether eager update needs to be explicitly set to false by default
+    this.autobase = new Autobase({
+      ...opts,
+      autostart: false
+    })
+    this.autobase.start({
+      unwrap: true,
+      apply: this.eventIndexesApply.bind(this),
+      view: (core) => new Hyperbee(core.unwrap(), {
+        ...opts,
+        extension: false
+      })
+    })
+
+    this.bus = new EventEmitter()
+
+    // Setup emitting on event emitter via hyperbee
+    this.eventStream = null
+    this.eventStreamRetry = null
+    this.autobase.once('append', this.setupEventStream.bind(this))
+  }
+
+  setupEventStream () {
+    this.eventStream = this.autobase.view.createHistoryStream({ live: true, gte: -1 })
+      .on('data', (node) => {
+        const { key, value } = node
+        const prefix = key.substring(0, 6)
+        if (prefix === 'event!') {
+          const eventObj = value
+          const eventDetails = { data: eventObj.data, timestamp: eventObj.timestamp }
+          this.bus.emit(eventObj.event, eventDetails)
+        }
+      })
+      .on('error', (err) => {
+        // Compensate for a non-await .get on LinearizedCore
+        // When the history stream reaches the end, hyperbee just request the
+        // next block assuming that the .get() will resolve when its available.
+        // The LinearizedCore implementation of Hypercore doesn't support this
+        // but instead has a retry X times setup which quickly gets exhausted
+        // when explicitly requesting a block out of bounds.
+        if (err.message === 'Linearization could not be rebuilt after 32 attempts') {
+          this.eventStreamRetry = setTimeout(this.setupEventStream.bind(this), 5 * 1000)
+        }
+      })
+  }
+
+  ready () {
+    return this.autobase.ready()
+  }
+
+  async eventIndexesApply (bee, batch) {
+    const b = bee.batch({ update: false })
+    for (const node of batch) {
+      const eventObj = this.valueEncoding.decode(node.value)
+      const { event, timestamp } = eventObj
+      const timestampMS = (new Date(timestamp)).getTime()
+
+      await Promise.all([
+        // By Time
+        b.put(['time', timestampMS, event, node.id, node.seq].join('!'), eventObj),
+        // By event
+        b.put(['event', event, timestampMS, node.id, node.seq].join('!'), eventObj)
+      ])
+    }
+
+    await b.flush()
+  }
+
+  on (event, cb) {
+    assert(typeof event === 'string', 'event must be a string')
+    assert(typeof cb === 'function', 'second argument must be a callback function')
+
+    this.bus.on(event, cb)
+    return this
+  }
+
+  async emit (event, ...args) {
+    assert(typeof event === 'string', 'event must be a string')
+    assert(this.autobase.localInput, 'No localInput hypercore provided')
+
+    await this.autobase.localInput.ready()
+
+    const data = enc(this.valueEncoding, {
+      event,
+      data: args,
+      timestamp: new Date()
+    })
+
+    return this.autobase.append(data)
+  }
+
+  async close () {
+    clearTimeout(this.eventStreamRetry)
+    this.eventStream.destroy()
+    await this.autobase.close()
+  }
+}
+
+function enc (e, v) {
+  if (v === undefined || v === null) return null
+  if (e !== null) return e.encode(v)
+  if (typeof v === 'string') return b4a.from(v)
+  return v
+}
---
+++ tests/event-bus.js
@@ -0,0 +1,90 @@
+import test from 'tape'
+import Corestore from 'corestore'
+import RAM from 'random-access-memory'
+
+import { EventBus } from '../index.js'
+
+async function asyncThrows (fn, err, t, message = 'throws') {
+  try {
+    await fn()
+    t.fail(message)
+  } catch (e) {
+    if (err && e.message.match(err)) {
+      t.pass(message)
+    } else if (!err) {
+      t.pass(message)
+    } else {
+      t.fail(message)
+      console.error(e)
+    }
+  }
+}
+
+test('EventBus', (t) => {
+  t.test('construction', (t) => {
+    const bus = new EventBus()
+
+    // Public properties
+    t.ok(bus.autobase, 'has autobase property')
+    t.equals(typeof bus.on, 'function', 'has `on` function')
+    t.equals(typeof bus.emit, 'function', 'has `emit` function')
+
+    t.end()
+  })
+
+  t.test('emit', async (t) => {
+    const corestore = new Corestore(RAM)
+    const errorEmitBus = new EventBus(corestore, { localInput: null })
+
+    await asyncThrows(async () => await errorEmitBus.emit('beep', 'foo', 2, 'baz'),
+      /No localInput hypercore provided/, t, 'throws when no localInput is defined')
+
+    // Normal use
+    const bus = new EventBus(corestore, {
+      valueEncoding: 'json',
+      localInput: corestore.get({ name: 'emitLocalInput' })
+    })
+    try {
+      await bus.emit('beep', 'foo', 2, 'baz')
+      t.pass('doesnt throw w/ normal use')
+    } catch (e) {
+      t.fail('doesnt throw w/ normal use')
+      console.error(e)
+    }
+
+    t.end()
+  })
+
+  t.test('on', async (t) => {
+    t.plan(2)
+
+    const corestore = new Corestore(RAM)
+
+    const errorEmitBus = new EventBus(corestore)
+    t.throws(() => errorEmitBus.on(), /event must be a string/, 'throws when no event is given')
+
+    // Normal use
+    const mine = corestore.get({ name: 'onLocalInput' })
+    const bus = new EventBus(corestore, {
+      eagerUpdate: true,
+      keyEncoding: 'utf-8',
+      valueEncoding: 'json',
+      inputs: [mine],
+      localInput: mine
+    })
+
+    const tasks = [
+      new Promise((resolve, reject) => {
+        bus.on('beep', () => {
+          t.pass('beep callback was fired')
+          resolve()
+        })
+      }),
+      bus.emit('beep')
+    ]
+
+    return Promise.all(tasks).then(bus.close.bind(bus))
+  })
+
+  t.end()
+})

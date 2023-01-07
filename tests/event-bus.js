4fb64ab99435b1b0483574f8bf84565e31994b31
Sean Zellmer <sean@lejeunerenard.com>
2023-01-06 22:34:59 -0600

    test: Write test for both eagerUpdate enabled & disabled
    Also wrote a test for support of a secondary index.

--- tests/event-bus.js
+++ tests/event-bus.js
@@ -1,6 +1,8 @@
 import test from 'tape'
 import Corestore from 'corestore'
 import RAM from 'random-access-memory'
+import Autobase from 'autobase'
+import Hyperbee from 'hyperbee'
 
 import { EventBus } from '../index.js'
 
@@ -58,22 +60,139 @@ test('EventBus', (t) => {
   })
 
   t.test('on', async (t) => {
-    t.plan(2)
+    t.test('w/ eagerUpdate false', async (t) => {
+      t.plan(2)
+
+      const corestore = new Corestore(RAM)
+
+      const errorEmitBus = new EventBus(corestore)
+      t.throws(() => errorEmitBus.on(), /event must be a string/,
+        'throws when no event is given')
+
+      // Normal use
+      const mine = corestore.get({ name: 'onLocalInput' })
+      const bus = new EventBus(corestore, {
+        eagerUpdate: false,
+        keyEncoding: 'utf-8',
+        valueEncoding: 'json',
+        inputs: [mine],
+        localInput: mine
+      })
+
+      const tasks = [
+        new Promise((resolve, reject) => {
+          bus.on('beep', () => {
+            t.pass('beep callback was fired')
+            resolve()
+          })
+        }),
+        bus.emit('beep'),
+        bus.emit('boop')
+      ]
+
+      await bus.autobase.view.update()
+
+      return Promise.all(tasks).then(bus.close.bind(bus))
+    })
+
+    t.test('w/ eagerUpdate true', async (t) => {
+      t.plan(2)
+
+      const corestore = new Corestore(RAM)
+
+      // Normal use
+      const mine = corestore.get({ name: 'onLocalInput' })
+      const mineOut = corestore.get({ name: 'onLocalOut' })
+      const bus = new EventBus(corestore, {
+        eagerUpdate: true,
+        keyEncoding: 'utf-8',
+        valueEncoding: 'json',
+        inputs: [mine],
+        localInput: mine,
+        localOutput: mineOut
+      })
+
+      // TODO: Seems to hang on the event stream...
+
+      const tasks = [
+        new Promise((resolve, reject) => {
+          bus.on('beep', () => {
+            t.pass('beep callback was fired')
+            resolve()
+          })
+        }),
+        bus.emit('beep'),
+        bus.emit('boop'),
+        new Promise((resolve, reject) => {
+          setTimeout(() => {
+            bus.on('after', () => {
+              t.pass('after callback was fired later')
+              resolve()
+            })
+          }, 10)
+        }),
+        new Promise((resolve, reject) => {
+          setTimeout(() => {
+            bus.emit('after', 1337)
+            resolve()
+          }, 50)
+        })
+      ]
+
+      return Promise.all(tasks).then(bus.close.bind(bus))
+    })
+  })
+
+  t.test('supports other indexes', async (t) => {
+    // t.plan(2)
 
     const corestore = new Corestore(RAM)
-
-    const errorEmitBus = new EventBus(corestore)
-    t.throws(() => errorEmitBus.on(), /event must be a string/,
-      'throws when no event is given')
 
     // Normal use
     const mine = corestore.get({ name: 'onLocalInput' })
     const bus = new EventBus(corestore, {
-      eagerUpdate: true,
+      eagerUpdate: false,
       keyEncoding: 'utf-8',
       valueEncoding: 'json',
       inputs: [mine],
       localInput: mine
+    })
+
+    const secondaryOptions = {
+      eagerUpdate: false,
+      inputs: [mine],
+      localInput: mine,
+      keyEncoding: 'utf-8',
+      valueEncoding: 'json'
+    }
+    const secondaryIndex = new Autobase({
+      ...secondaryOptions,
+      autostart: false
+    })
+
+    secondaryIndex.start({
+      unwrap: true,
+      apply: async (bee, batch) => {
+        const b = bee.batch({ update: false })
+        for (const node of batch) {
+          const eventObj = JSON.parse(node.value)
+          const { timestamp, data } = eventObj
+          const timestampMS = (new Date(timestamp)).getTime()
+
+          const firstArg = data[0]
+
+          await Promise.all([
+            // By event
+            b.put(['props', Object.keys(firstArg).join(','), timestampMS, node.id, node.seq].join('!'), eventObj)
+          ])
+        }
+
+        await b.flush()
+      },
+      view: (core) => new Hyperbee(core.unwrap(), {
+        ...secondaryOptions,
+        extension: false
+      })
     })
 
     const tasks = [
@@ -83,10 +202,39 @@ test('EventBus', (t) => {
           resolve()
         })
       }),
-      bus.emit('beep')
+      bus.emit('beep', { boop: true }),
+      bus.emit('foo', { fizz: 'buzz' })
     ]
 
-    return Promise.all(tasks).then(bus.close.bind(bus))
+    await Promise.all(tasks)
+
+    await bus.autobase.view.update()
+    await secondaryIndex.view.update()
+
+    const secondaryKeys = []
+    for await (const node of secondaryIndex.view.createReadStream({ gt: 'props!', lt: 'props"' })) {
+      secondaryKeys.push(node.key.toString())
+    }
+
+    t.match(secondaryKeys[0], /props!boop/, 'found boop key')
+
+    const foundEvents = []
+    for await (const node of bus.autobase.view.createReadStream({ gt: 'event!', lt: 'event"' })) {
+      foundEvents.push(node.key.toString())
+    }
+    t.match(foundEvents[0], /event!beep/, 'found original beep')
+    t.match(foundEvents[1], /event!foo/, 'found original foo')
+
+    const foundTimes = []
+    for await (const node of bus.autobase.view.createReadStream({ gt: 'time!', lt: 'time"' })) {
+      foundTimes.push(node.key.toString())
+    }
+    t.match(foundTimes[0], /time!\d+!beep/, 'found original beep time')
+    t.match(foundTimes[1], /time!\d+!foo/, 'found original foo time')
+
+    bus.close()
+
+    t.end()
   })
 
   t.end()
